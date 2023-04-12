42a139e175b825e21eb971fe85389861eefd09d7
wangjie <1641540482@qq.com>
2023-04-12 16:07:47 +0800

    首次提交

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="time_utils",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
     version="1.1.0",  # 程序版本
+    py_modules=['time_utils'],
     author="WangJie",  # 项目作者
-    author_email="xxxx@qq.com",  # 作者邮件
+    author_email="1641540482@qq.com",  # 作者邮件
     description="提供了各种时间转换的方法",  # 项目的一句话描述
     long_description=long_description,  # 加长版描述
     long_description_content_type="text/markdown",  # 描述使用Markdown
-    url="https://github.com/wangjie-jason",  # 项目地址
+    url="https://github.com/wangjie-jason/time_utils",  # 项目地址
     packages=setuptools.find_packages(),  # 无需修改
     classifiers=[
         "Programming Language :: Python :: 3",  # 使用Python3
